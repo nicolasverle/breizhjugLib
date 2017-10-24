cdffe5e23fc4b682faafbcb865097cd9b41e01eb
nicolasverle <nicolas.verle@gmail.com>
2017-10-24 17:19:18 +0200

    fix missing sonar preview mode

--- src/com/zenika/tz/demo/build/JavaBuildStrategy.groovy
+++ src/com/zenika/tz/demo/build/JavaBuildStrategy.groovy
@@ -22,7 +22,7 @@ class JavaBuildStrategy extends CommandWrapper implements BuildStrategy {
     @Override
     def analyze(QualityRequirements requirements = null) {
         withMaven(MAVEN_ALIAS) {
-            sh("mvn sonar:sonar -Dsonar.report.export.path=report.json -Dsonar.issuesReport.console.enable=true -Dsonar.host.url=${SONAR_URL}")
+            sh("mvn sonar:sonar -Dsonar.analysis.mode=preview -Dsonar.report.export.path=report.json -Dsonar.issuesReport.console.enable=true -Dsonar.host.url=${SONAR_URL}")
             if(requirements) {
                 def json = parseFile("report.json", ResultFormatEnum.JSON)
                 echo("Will parse : ${JsonOutput.prettyPrint(json.toString())}")

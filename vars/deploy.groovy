2c779cb9cd919e59674b92adcfc3ce069ff52e48
nicolasverle <nicolas.verle@gmail.com>
2017-11-03 09:04:21 +0100

    Refacto de derriere les fagots

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -3,19 +3,14 @@ import com.zenika.tz.demo.deploy.DockerHandler
 import groovy.transform.Field
 
 @Field String host
-@Field int port
 
 def call(Map params, Closure body) {
 
     if(!params.host) {
         error("Host must be set when deploying")
     }
-    if(!params.port) {
-        error("Port must be set when deploying")
-    }
 
     host = params.host
-    port = params.port
 
     node(PipelineContextHolder.buildStrategy.getNodesLabel()) {
         stage("Deploying application") {
@@ -31,11 +26,6 @@ def call(Map params, Closure body) {
 }
 
 def dockerd(Map params) {
-    def ports = [[host: port, container: 8080]]
-    if(params.ports) {
-        ports.addAll(params.ports)
-    }
-
     String opts = "-m 512m"
     if(params.opts) {
         opts += params.opts
@@ -44,5 +34,5 @@ def dockerd(Map params) {
     DockerHandler handler = new DockerHandler(host)
 
     handler.deploy(params.image, params.tag,
-            ports, params.volumes, opts)
+            params.ports, params.volumes, opts)
 }

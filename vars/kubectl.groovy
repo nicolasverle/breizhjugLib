e0b9a0421bdcbf312816c25d890fdf3b74e92c5d
nicolasverle <nicolas.verle@gmail.com>
2017-12-07 13:47:11 +0100

    Debuging...

--- vars/kubectl.groovy
+++ vars/kubectl.groovy
@@ -3,9 +3,14 @@ import com.zenika.tz.demo.deploy.Kubernetes
 
 def call(Map params, Closure body) {
 
-    Kubernetes kubernetes = new Kubernetes(params.namespace)
-    PipelineContextHolder.kubernetes = kubernetes
-    kubernetes.initContext()
-    body()
-    //PipelineContextHolder.kubernetes.apply()
+    try {
+        Kubernetes kubernetes = new Kubernetes(params.namespace)
+        PipelineContextHolder.kubernetes = kubernetes
+        kubernetes.initContext()
+        body()
+        //PipelineContextHolder.kubernetes.apply()
+    } catch (err) {
+        error(err.dump())
+    }
+
 }

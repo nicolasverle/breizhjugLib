9ce7f5ff99472438f718c8e806a207aee7cb5783
nicolasverle <nicolas.verle@gmail.com>
2017-12-07 17:17:11 +0100

    Add Ingress

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,12 +1,11 @@
 import com.zenika.tz.demo.PipelineContextHolder
-import groovy.transform.Field
 
 def call(Map params, Closure body) {
 
     PipelineContextHolder.deployContext.appName = params.appName
     PipelineContextHolder.deployContext.port = params.appPort
 
-    node(PipelineContextHolder.buildStrategy.getNodesLabel()) {
+    node("master") {
         stage("Deploying application") {
             if(body) {
                 body.resolveStrategy = Closure.DELEGATE_FIRST

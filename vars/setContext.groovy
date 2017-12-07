0ad631bf532de51bb93fc789d8814d45aa377d92
nicolasverle <nicolas.verle@gmail.com>
2017-12-07 12:13:10 +0100

    Debuging...

--- vars/setContext.groovy
+++ vars/setContext.groovy
@@ -1,6 +1,7 @@
 import com.zenika.tz.demo.PipelineContextHolder
 
 def call() {
+    echo("setting context...")
     if(!PipelineContextHolder.script) {
         PipelineContextHolder.init(this)
     }
